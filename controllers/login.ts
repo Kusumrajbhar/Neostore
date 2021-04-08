68bf70907db31c0873dfdd264f62be04b9a850e7
kusumrajbhar <kusum.rajbhar@neosoftmail.com>
2021-04-08 13:56:09 +0530

    login api

--- controllers/login.ts
+++ controllers/login.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-//import client from '../models/database';
 import { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import Joi, { string } from 'joi';
 import Customer from '../models/customerRegistrationSchema';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 app.use(express.json());
@@ -13,17 +14,48 @@ const schema = Joi.object({
     password: Joi.string().regex(/^[A-Z a-z 0-9]{3,15}$/).required(),
 });
 
-const customerLogin = (req: request , res: response) => {
+const customerLogin = (req: Request, res: Response) => {
     try {
-        const customerRegistration = new Customer({
-            email: req.body.email,
-            password: req.body.password,
+        const loginEmail = req.body.email;
+        const loginPassword = req.body.password;
+
+        Customer.findOne({ email: loginEmail }, (err, result) => {                  //findOne to get one document
+            console.log('result', result);
+            const validate = schema.validate(req.body);
+            if (validate.error) {
+                res.status(400).json({ success: false, message: validate.error.details[0].message, data: [] });
+            }
+            bcrypt.compare(loginPassword, result.password, (err, matchedData) => {
+                if (err) {
+                    console.log(" wrong password");
+                    res.status(400).json({ message: 'enter correct password' });
+                }
+                else
+                    if (matchedData) {
+                        console.log('logged In successfully', matchedData);
+                        const token = jwt.sign({ id: result.id, email: result.email }, "mynameiskusumrajbhar");
+                        console.log("token : ", token);
+                       // let text = 'result';
+                        let objResult = JSON.parse('result'); 
+                       // let objResult = Object.entries(result);
+                        console.log('objResult', objResult)
+                        let responseObject = objResult;
+                        responseObject["token"] = token;
+                        delete responseObject["password"];
+                        delete responseObject["id"]; 
+                        res.status(200).json({success: true, status: 200, message: 'logged in successfully', Customer_Details: responseObject});
+                    }
+                    else {
+                        res.status(400).json({success: false, status: 400, message: "Please enter correct password"})
+                    }
+            })
+
         })
 
-
     }
-    catch {
-
+    catch (err) {
+        console.log(err);
+        res.status(400).json({ success: false, message: 'Not Registered User', data: err.message });
     }
 }
 
@@ -39,14 +71,10 @@ const customerLogin = (req: request , res: response) => {
 
 //       }
 //       catch {
-
 //       }
 //   })  
-
-  
-
 //}
 
-// export = {
-//     customer_login
-// }
+export = {
+    customerLogin
+}
